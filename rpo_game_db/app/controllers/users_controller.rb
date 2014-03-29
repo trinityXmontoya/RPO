722a80cb094e504b47336119ef436a5c50ac50b1
trinityXmontoya <trinityXmontoya@gmail.com>
2014-03-29 15:32:22 -0400

    users controller

---
+++ rpo_game_db/app/controllers/users_controller.rb
@@ -0,0 +1,56 @@
+class UsersController < ApplicationController
+
+  before_action :authenticate, only: [:edit, :update, :destroy]
+
+  def index
+    @users=User.all
+  end
+
+  def show
+    @user=User.find(params[:id])
+  end
+
+  def new
+    @user=User.new
+  end
+
+  def create
+    @user= User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @user=User.find(params[:id])
+    if current_user == @user
+      render 'edit'
+    else
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if current_user == @user
+      @user.update(user_params)
+      redirect_to @user
+    else
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    @user=User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+  def user_params
+      params.require(:user).permit(:username, :email, :password, :password_confirmation, :photo_url, :character_id, :time_played, :level_id)
+  end
+
+end
