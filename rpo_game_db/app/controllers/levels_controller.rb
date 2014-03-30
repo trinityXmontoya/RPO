c060a0562f531af61c5c4274581be6d251112b5f
trinityXmontoya <trinityXmontoya@gmail.com>
2014-03-30 06:39:55 -0400

    beginnings of gameplay, have recognized usefulness of adding more model attributes

--- rpo_game_db/app/controllers/levels_controller.rb
+++ rpo_game_db/app/controllers/levels_controller.rb
@@ -18,6 +18,44 @@ class LevelsController < ApplicationController
     @user=User.find(session[:user_id])
     @level=Level.find(params[:id])
     @user.levels << @level
+    if @level.id == 1
+      @content="Do you climb the mountain or hill?"
+      @option1="Climb Mountain."
+      @option2="Climb Hill"
+    elsif @level.id == 2
+      @option1="Scale Tree"
+      @option2="Scale Building"
+      @content="Do you scale the tree or the building?"
+    end
   end
+
+  def begin
+    @user=User.find(session[:user_id])
+    @level=Level.find(params[:id])
+    if @level.id == 1 && params[:]==
+      @content="Do you climb the mountain or hill?"
+      @option1="Climb Mountain."
+      @option2="Climb Hill"
+    else @level.id == 1 &&
+      @content="Do you climb the mountain or hill?"
+      @option1="Climb Mountain."
+      @option2="Climb Hill"
+    elsif @level.id == 2
+      @option1="Scale Tree"
+      @option2="Scale Building"
+      @content="Do you scale the tree or the building?"
+    end
+
+
+  end
+
+  def game
+    @game=Game.find(params[:game_id])
+    redirect_to game_path(@game)
+  end
+
+  def end
+  end
+
 
 end
--- rpo_game_db/config/routes.rb
+++ rpo_game_db/config/routes.rb
@@ -8,9 +8,13 @@ get '/logout', to: 'sessions#destroy'
 get '/welcome', to: 'main#welcome'
 
 resources :characters, only: :index, shallow:true do
-  resources :levels, only: [:index, :show]
+  resources :levels, only: [:index, :show], shallow:true do
+    resources :games, only: :show
+  end
 end
 
+get '/levels/:id/begin', to: 'levels#begin'
+get '/levels/:id/game/:game_id', to: 'levels#game'
 
 root to: 'users#index'
 
