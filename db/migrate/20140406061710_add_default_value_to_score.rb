20debb0ef5914bacc01084adaa70096ebb2b4623
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-06 02:19:41 -0400

    add default value of 0 to user score attribute

---
+++ db/migrate/20140406061710_add_default_value_to_score.rb
@@ -0,0 +1,5 @@
+class AddDefaultValueToScore < ActiveRecord::Migration
+  def change
+    change_column :users, :score, :integer, :default => 0
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140331122308) do
+ActiveRecord::Schema.define(version: 20140406061710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,7 +92,7 @@ ActiveRecord::Schema.define(version: 20140331122308) do
     t.string   "photo_url"
     t.integer  "time_played"
     t.integer  "level_id"
-    t.integer  "score"
+    t.integer  "score",           default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
   end
